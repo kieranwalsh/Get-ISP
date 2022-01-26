c27f51a7496c962f58c0c2b0ef0956e59be1027b
kieranwalsh <walshk@pm.me>
2022-01-26 19:06:43 +0000

    Initial commit.

---
+++ Get-ISP.ps1
@@ -0,0 +1,63 @@
+﻿<#
+    .SYNOPSIS
+    Lists the ISP for any public IP address.
+
+    .DESCRIPTION
+    Scans AD for DPM servers, connects to them and gathers information about the DPM setup and all jobs. This information is compiled
+    into HTML which becomes the body of an email report.
+
+    .PARAMETER PublicIPs
+    Use this parameter to specify a list of public IP addresses to scan. If this parameter is not specified, the script will scan the public IP of the device you run it from.
+
+    .PARAMETER ISPOnly
+    Instead of outputting the results in a table form, ISPOnly will list the ISP name only. Useful if you need to supply the data to another script or RMM.
+
+    .EXAMPLE
+    C:\PS> .\Get-ISP.ps1
+    Returns the public IP and the ISP of the device you are running the script from.
+
+    .EXAMPLE
+    C:\PS> .\Get-ISP.ps1 -PublicIPs 8.8.8.8,1.1.1.1
+    Returns the public IP(s) and the ISP(s) of the IP Addresses supplied. For example:
+    IP: 8.8.8.8     ISP: Google LLC
+    IP: 1.1.1.1     ISP: APNIC and CloudFlare DNS Resolver Project
+
+    .EXAMPLE
+    C:\PS> .\Get-ISP.ps1 -ISPOnly 8.8.8.8
+    Returns the ISP of the IP Address supplied without listing the IP in a table. For example:
+    Google LLC
+
+    .NOTES
+    Filename: Get-ISP.ps1
+    Contributors: Kieran Walsh
+    Created: 2022-01-26
+    Last Updated: 2022-01-26
+    Version: 0.01.0
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter()]
+    [string[]]$PublicIPs,
+    [switch]$ISPOnly
+)
+
+if(-not($PublicIPs))
+{
+    $PublicIPs = (Invoke-WebRequest -Uri http://ifconfig.me/ip -TimeoutSec 60).Content.Trim()
+}
+foreach($PublicIP in $PublicIPs)
+{
+    $SearchURL = "https://api.iplocation.net/?ip=$PublicIP"
+    $ISP = ((Invoke-WebRequest -Uri $SearchURL -UseBasicParsing).content -replace '{', '' -replace '}', '' -replace '"', '' -split ',' | Where-Object {
+            $_ -match 'isp:'
+        }).replace('isp:', '')
+    if($IspOnly)
+    {
+        $ISP
+    }
+    Else
+    {
+        Write-Host "IP: $PublicIP `tISP: $ISP"
+    }
+}
